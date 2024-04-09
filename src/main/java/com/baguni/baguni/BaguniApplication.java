93e4800b417c38526d01e328d0d1ca9c909392e9
naahnoey <naahyin20@sookmyung.ac.kr>
2024-04-09 13:46:03 +0900

    first commit

---
+++ src/main/java/com/baguni/baguni/BaguniApplication.java
@@ -0,0 +1,13 @@
+package com.baguni.baguni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaguniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaguniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baguni/baguni/BaguniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baguni.baguni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaguniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
